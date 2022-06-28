94cded6308a612b0e7192ba0807c530970d32372
Sergey <solvman@gmail.com>
2022-06-27 21:37:33 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import ReactMarkdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { materialLight } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import remarkBreaks from "remark-breaks";
 
 function App() {
+  const [text, setText] = useState("");
+  const MarkdownComponents = {
+    code({
+      node,
+      inline,
+      className = "language-javascript",
+      children,
+      ...props
+    }) {
+      const match = /language-(\w+)/.exec(className || "");
+      return !inline && match ? (
+        <SyntaxHighlighter
+          style={materialLight}
+          language={match[1]}
+          PreTag="div"
+          {...props}
+        >
+          {String(children).replace(/\n$/, "")}
+        </SyntaxHighlighter>
+      ) : (
+        <code className={className} {...props}>
+          {children}
+        </code>
+      );
+    },
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>Markdown Previewer</header>
+      <main>
+        <textarea
+          id="editor"
+          value={text}
+          onChange={(event) => setText(event.target.value)}
+        ></textarea>
+        <div id="preview">
+          <ReactMarkdown
+            children={text}
+            components={MarkdownComponents}
+            remarkPlugins={[remarkBreaks]}
+          />
+        </div>
+      </main>
     </div>
   );
 }
